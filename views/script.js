23120d5dc78549e1d2d79173bb88cf7ccccf5fac
Will Munn <willmunn@hotmail.com>
2011-11-07 23:18:40 +0000

    captured on/off state

--- views/script.js
+++ views/script.js
@@ -1,11 +1,16 @@
 $(function(){
-	var ticks = $('.tick');
-
-	$('.tick').click(function(evt){
-		var element = $(evt.currentTarget);
-		toggle(element);
-	});
-
+	var ticks = $('.kick .tick');
+	var hits = [];
+	
+	for (var i=0; i<ticks.length; i++) {
+		hits[i] = 0;
+		$(ticks[i]).click(function(x){
+			return function  (){
+				hits[x] = hits[x] === 0 ? 1 : 0;
+				toggle($(this));
+			};
+		}(i));
+	}
 
 	function toggle (element){
 		var isOn = element.css('background-color') !== 'rgb(255, 0, 0)';
