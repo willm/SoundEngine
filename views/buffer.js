34733cccd086ae35ce5f438101b77643a7a7a40c
Will Munn <willmunn@hotmail.com>
2011-11-07 23:30:53 +0000

    play array of kicks

--- views/buffer.js
+++ views/buffer.js
@@ -3,6 +3,7 @@ $(function (){
 	var list;
 	var context = new webkitAudioContext();
 	var isPlaying = false;
+	var kicks = [1,1,1,0,1,0,1,0];
 	
 	var bufferLoader = new BufferLoader(
 		context,
@@ -41,16 +42,22 @@ $(function (){
 		for (var bar = 0; bar < 16; bar++) {
 			var time = startTime + bar * 8 * eighthNoteTime;
 			// Play the bass (kick) drum on beats 1, 5
-			playSound(kick, time);
-			playSound(kick, time + 5 * eighthNoteTime);
+			/*playSound(kick, time);
+			playSound(kick, time + 5 * eighthNoteTime);*/
+			for (var i=0; i < kicks.length; i++) {
+				if(kicks[i]){
+					playSound(kick,(time)+i * eighthNoteTime);
+				}
+			}
+			//if(kicks)
 
 			// Play the snare drum on beats 3, 7
-			playSound(snare, time + 2 * eighthNoteTime);
+			/*playSound(snare, time + 2 * eighthNoteTime);
 			playSound(snare, time + 6 * eighthNoteTime);
 			
 			for (var i = 0; i < 8; ++i) {
 				playSound(hihat, time + i * eighthNoteTime);
-			}
+			}*/
 		}
 	}
 })
