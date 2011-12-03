80d0f179d358877c5e44368857080967bfe12850
Will Munn <willmunn@hotmail.com>
2011-12-03 12:00:50 +0000

    load channel UI in script

--- views/scripts/ChannelDiv.js
+++ views/scripts/ChannelDiv.js
@@ -1,11 +1,40 @@
-function ChannelRow(numberOfTicks){
-	var channelDiv = $('<div>').attr('class', 'row channel');
-	channelDiv.append(
-		$('<p>').attr('class','sample_path').text('new-sound.wav')
+function ChannelDiv (numberOfTicks){
+	this.channel;
+	this.ticks = numberOfTicks;
+	this.div = $('<div>').attr('class', 'row channel');
+	
+	this.div.append(
+		$('<p>').attr('class','sample_path').text('samples/Clique-Perc2-01.wav')
 	);
 	
-	for(var i=0; i<numberOfTicks; i++){
-		channelDiv.append($('<div>').attr('class', 'tick'));
+	for(var i=0; i<this.ticks; i++){
+		this.div.append($('<div>').attr('class', 'tick'));
 	}
-	$('#channels').append(channelDiv);
+	$('#channels').append(this.div);
 }
+
+ChannelDiv.prototype.hookUpChannel = function  (){
+
+
+				var tickElements = this.div.children('.tick'),
+					samplePath = this.div.children('.sample_path').text();
+				this.channel = new Channel(this.ticks);
+				this.channel.loadBuffer(samplePath);
+				for (var j=0; j<this.ticks; j++) {
+					$(tickElements[j]).click(function(channel, tick){
+						return function  (){
+							channel.setHit(tick);
+							console.log('channel: '+channel+'tick: '+tick);
+							toggle($(this));
+						};
+					}(this.channel,j));
+				}
+			}
+			
+this.toggle = function (element){
+		var isOn = element.css('background-color') !== 'rgb(255, 0, 0)';
+		if(isOn)
+			element.css('background-color', 'red');
+		else 
+			element.css('background-color', 'transparent');
+	}
