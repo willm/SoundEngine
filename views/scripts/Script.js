36702569466099cfc2c5026dd13fb6a879ac630d
Will Munn <willmunn@hotmail.com>
2011-12-16 23:53:44 +0000

    simplified script.js

--- views/scripts/Script.js
+++ views/scripts/Script.js
@@ -7,17 +7,12 @@ $(function(){
 	
 	new AddTrackButton(channelElements, channels);
 	
-	hookUpChannels();
-	
-	function hookUpChannels (){
-		for (var i=0; i<channelElements.length; i++ ) {
-			channels.push(channelElements[i].channel);
-			console.log(channels[i]);
-		}
-		soundEngine = new SoundEngine(channels);
-		new PlayButton(soundEngine);
-		new TempoSelector(soundEngine);
+	for (var i=0; i<channelElements.length; i++ ) {
+		channels.push(channelElements[i].channel);
 	}
+	soundEngine = new SoundEngine(channels);
+	new PlayButton(soundEngine);
+	new TempoSelector(soundEngine);
 	
 });
 
