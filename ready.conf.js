a5114146976bc5a84860aace25ceea0ebd47395a
Will Munn <willmunn@hotmail.com>
2012-02-17 01:12:21 +0000

    checking again

---
+++ ready.conf.js
@@ -0,0 +1,7 @@
+{ 
+   src : "./views/scripts", // the source dir of js files
+   dest : "./minified", // the destination of your minified files
+   minifiedExtension : "min", // Extension of the minified file
+   runGCompiler : true, // if should run GoogleCompiler
+   aggregateTo : "./minified/all.js" // Which file to aggregate all javascript files
+}
