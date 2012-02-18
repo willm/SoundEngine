b5d759bbe637d58faee20f247681de467b94c522
Will Munn <willmunn@hotmail.com>
2012-02-18 22:27:51 +0000

    moved hit UI code out of channel div to hit

--- views/scripts/ChannelDiv.js
+++ views/scripts/ChannelDiv.js
@@ -11,33 +11,9 @@ function ChannelDiv (numberOfTicks){
 	this.div.append(controls);
 	
 	for(var i=0; i<this.ticks; i++){
-		this.div.append($('<div>').attr('class', 'tick'));
+		this.div.append(this.channel.hits[i].div);
 	}
 	this.div.append($('<br>'));
 	this.div.append(new PitchDiv(this.channel));
-	this.hookUpChannel();
 	$('#channels').append(this.div);
 }
-
-ChannelDiv.prototype.hookUpChannel = function  (){
-	var tickElements = this.div.children('.tick');
-		for (var i=0; i<this.ticks; i++) {
-			$(tickElements[i]).click(function(channelDiv, tick){
-				return function  (){
-					channelDiv.channel.setHit(tick);
-					console.log('channel: '+channelDiv.channel+'tick: '+tick);
-					channelDiv.toggle($(this));
-					$('.channel').css('border', 'none');
-					channelDiv.div.css('border', '1px dashed');
-				};
-			}(this,i));
-		}
-	}
-
-ChannelDiv.prototype.toggle = function (element){
-		var isOn = element.css('background-color') !== 'rgb(255, 0, 0)';
-		if(isOn)
-			element.css('background-color', 'red');
-		else 
-			element.css('background-color', 'transparent');
-	}
--- views/scripts/Hit.js
+++ views/scripts/Hit.js
@@ -1,6 +1,7 @@
 var Hit = function  (){
 	this.isOn = false;
 	this.pitch = 1;
+	this.div = this.setUpDiv();
 }
 
 Hit.prototype.set = function  (){
@@ -9,5 +10,22 @@ Hit.prototype.set = function  (){
 
 Hit.prototype.changePitch = function  (pitch){
 	this.pitch = pitch;
+}
+
+Hit.prototype.toggle = function (){
+		if(this.isOn)
+			this.div.css('background-color', 'red');
+		else 
+			this.div.css('background-color', 'transparent');
+	}
+	
+Hit.prototype.setUpDiv = function  (){
+	return $('<div>').attr('class', 'tick')
+		.click(function(hit){
+				return function  (){
+					hit.set();
+					hit.toggle();
+				};
+			}(this));
 }
 
