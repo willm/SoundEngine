d6d9f117612476385fa46c556c3641b418b46214
Will Munn <willmunn@hotmail.com>
2012-02-18 23:09:39 +0000

    keep UI code out of Hit logic!

--- views/scripts/Hit.js
+++ views/scripts/Hit.js
@@ -11,20 +11,16 @@ Hit.prototype.set = function  (){
 Hit.prototype.changePitch = function  (pitch){
 	this.pitch = pitch;
 }
-
-Hit.prototype.toggle = function (){
-		if(this.isOn)
-			this.div.css('background-color', 'red');
-		else 
-			this.div.css('background-color', 'transparent');
-	}
 	
 Hit.prototype.setUpDiv = function  (){
 	return $('<div>').attr('class', 'tick')
 		.click(function(hit){
 				return function  (){
 					hit.set();
-					hit.toggle();
+					if(hit.isOn)
+						$(this).css('background-color', 'red');
+					else 
+						$(this).css('background-color', 'transparent');
 				};
 			}(this));
 }
