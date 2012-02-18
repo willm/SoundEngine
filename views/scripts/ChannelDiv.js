95b78822713d8ae9bb683dbc946b177d1d772ffe
Will Munn <willmunn@hotmail.com>
2012-02-18 22:33:54 +0000

    cleaned up controls

--- views/scripts/ChannelDiv.js
+++ views/scripts/ChannelDiv.js
@@ -1,14 +1,10 @@
-function ChannelDiv (numberOfTicks){
+var ChannelDiv = function  (numberOfTicks){
 	this.channel;
 	this.ticks = numberOfTicks;
 	this.div = $('<div>').attr('class', 'row channel');
 	this.channel = new AudioletChannel(this.ticks);
-	var controls = $('<div class="controls">')
-		.css('float', 'left');
-	controls.append(new SampleList(this.channel));
-	controls.append($('<br><br>'));
-	controls.append(new VolumeSlider(this.channel));
-	this.div.append(controls);
+	
+	this.div.append(this.getControls());
 	
 	for(var i=0; i<this.ticks; i++){
 		this.div.append(this.channel.hits[i].div);
@@ -16,4 +12,13 @@ function ChannelDiv (numberOfTicks){
 	this.div.append($('<br>'));
 	this.div.append(new PitchDiv(this.channel));
 	$('#channels').append(this.div);
+}
+
+ChannelDiv.prototype.getControls = function  (){
+	var controls = $('<div class="controls">')
+		.css('float', 'left');
+	controls.append(new SampleList(this.channel));
+	controls.append($('<br><br>'));
+	controls.append(new VolumeSlider(this.channel));
+	return controls;
 }
