fccf3b4ddd73d9d14b3fbc42fdc9805f5fba8839
Will Munn <willmunn@hotmail.com>
2012-02-19 23:04:25 +0000

    moved beatlight div to metronome channel

--- views/scripts/MetronomeChannel.js
+++ views/scripts/MetronomeChannel.js
@@ -1,14 +1,15 @@
 function MetronomeChannel (numberOfTicks){
+	this.numberOfTicks = numberOfTicks;
 	this.hits = [];
+	this.div = this.setUpDiv();
 	for (var i = 0; i<numberOfTicks; i++) {
 		this.hits[i] = {position : i};
 	}
 	this.playEvent;
-	this. audiolet = SingleAudiolet.getInstance();
+	this.audiolet = SingleAudiolet.getInstance();
 }
 	
 MetronomeChannel.prototype.play = function (){
-	console.log(this.hits);
 	var pattern = new PSequence(this.hits, Infinity);
 		
 	this.playEvent = this.audiolet.scheduler.play([pattern],0.25,
@@ -21,3 +22,25 @@ MetronomeChannel.prototype.play = function (){
 MetronomeChannel.prototype.stop = function  (){
 	this.audiolet.scheduler.stop(this.playEvent);
 }
+
+MetronomeChannel.prototype.setUpDiv = function (){
+	var div = $('<div>').attr('class', 'row channel'),
+		lightUp = function (beat){
+			$(div.children(".tick")).css('background-color', 'transparent');
+			$(div.children(".tick")[beat]).css('background-color', 'yellow');
+		};
+		
+	div.append($('<div>').css('width', '200px').css('float','left').text('Your Song:'));
+	for(var i=0; i<this.numberOfTicks; i++){
+		div.append($('<div>').attr('class', 'tick'));
+	}
+	lightUp(0);
+	$('#channels').append(div);
+	
+	$(document).bind('nextBeat', function  (evt,data){
+			lightUp(data);
+		}.bind(this));
+	return div;
+}
+
+
