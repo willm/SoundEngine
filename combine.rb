22a59e3be857ce39ecd79770b34591b9d4a3969d
Will Munn <willmunn@hotmail.com>
2012-02-19 22:39:26 +0000

    moved combine.rb into def

--- combine.rb
+++ combine.rb
@@ -1,8 +1,12 @@
-dir = 'views/scripts/master'
-Dir.mkdir(dir) unless File.exists?(dir)
-files = Dir.glob('views/scripts/*.js');
-files = files + Dir.glob('views/scripts/UI/*.js');
-filesString = ''
-files.each { |file| filesString = filesString + ' ' + file
-}
-exec ('cat ' + filesString + '> '+ dir + '/master.js')
+def combine_client_files
+	dir = 'views/scripts/master'
+	Dir.mkdir(dir) unless File.exists?(dir)
+	files = Dir.glob('views/scripts/*.js');
+	files = files + Dir.glob('views/scripts/UI/*.js');
+	filesString = ''
+	files.each { |file| filesString = filesString + ' ' + file
+	}
+	exec ('cat ' + filesString + '> '+ dir + '/master.js')
+end
+
+combine_client_files
