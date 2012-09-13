526616c106c61991df358df63c2a0b67bd4afb4b
Will Munn <willmunn@hotmail.com>
2012-09-14 00:51:52 +0100

    concatonated js files in js rather than shelling out to cat

--- jakefile.js
+++ jakefile.js
@@ -1,34 +1,36 @@
-var	fs = require('fs'),
+var fs = require('fs'),
 	exec = require('child_process').exec,
 	sys = require('sys'),
 	path = require('path');
 
-	var getJsFiles = function(dir){
-		var jsFiles = '',
-			files = fs.readdirSync(dir);
+var getJsFiles = function(dir){
+	var combinedJsFiles = '',
+		files = fs.readdirSync(dir);
 
-		files.forEach(function(file, i){
+	files.forEach(function(file, i){
 			if(path.extname(file) === '.js')
-				jsFiles += dir + file + ' ';
+				combinedJsFiles += fs.readFileSync(dir + file).toString();
 		});
-		return jsFiles;
-	};
+	return combinedJsFiles;
+};
+
+var combineAllJsFiles = function(outDir){
+	var dir = 'views/scripts/',
+		allJsFiles = '';
+	allJsFiles = getJsFiles(dir) + getJsFiles(dir+'UI/');
+	fs.writeFileSync(outDir +'/master.js',allJsFiles);
+}
+
+var createIfNotExists = function(dir,cb){
+	fs.exists(dir,function(exists){
+		if(!exists)
+			fs.mkdirSync(dir);
+		cb(dir);
+	});
+}
 
 desc('concatonate client scripts');
 task('combine', [], function(){
-	var dir = 'views/scripts/',
-		allJsFiles = [],
-		filesToCombine = '';
-
-	fs.exists(dir,function(exists){
-		if(exists)
-			fs.mkdir(dir);
-	});
-
-	allJsFiles = getJsFiles(dir) + getJsFiles('views/scripts/UI/');
-	exec ('cat ' +  allJsFiles + ' > ' + dir + 'master/master.js',
-	function (error, stdout, stderr) {
-		console.log(stdout);
-		console.log(stderr);
-	});
+	createIfNotExists('views/scripts/master/',combineAllJsFiles);
 })
+
